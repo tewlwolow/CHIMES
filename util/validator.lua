9e65d72ab38e5a5fabfd9cd7fc06a1cc721f5ceb
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-10 01:20:53 +0200

    Test-print chart name

--- util/validator.lua
+++ util/validator.lua
@@ -1,21 +1,22 @@
 local validator = {}
 
 function validator.validate(instance)
-	local function walk(chartTable, schemaTable)
-		for k, v in pairs(chartTable) do
-			print(k)
-			print(v)
-			if type(chartTable[k]) == schemaTable[k].type then
-				print("good")
-			else
-				print("not good")
-			end
-			if type(v) == "table" then
-				walk(v, schemaTable[k])
-			end
-		end
-	end
-	walk(instance.chart, instance.schema)
+	mwse.log("Validating chart: " .. instance.chart.name)
+	-- local function walk(chartTable, schemaTable)
+	-- 	for k, v in pairs(chartTable) do
+	-- 		print(k)
+	-- 		print(v)
+	-- 		if type(chartTable[k]) == schemaTable[k].type then
+	-- 			print("good")
+	-- 		else
+	-- 			print("not good")
+	-- 		end
+	-- 		if type(v) == "table" then
+	-- 			walk(v, schemaTable[k])
+	-- 		end
+	-- 	end
+	-- end
+	-- walk(instance.chart, instance.schema)
 end
 
 return validator
