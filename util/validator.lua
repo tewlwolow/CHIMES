f67fbf4f859ed8aabd95ec57db8f859397f3196b
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-10 00:59:23 +0200

    WIP validator in

--- util/validator.lua
+++ util/validator.lua
@@ -1,8 +1,21 @@
 local validator = {}
 
 function validator.validate(instance)
-	print(instance.schema.data.item.strings.type)
-	print(instance.chart.name)
+	local function walk(chartTable, schemaTable)
+		for k, v in pairs(chartTable) do
+			print(k)
+			print(v)
+			if type(chartTable[k]) == schemaTable[k].type then
+				print("good")
+			else
+				print("not good")
+			end
+			if type(v) == "table" then
+				walk(v, schemaTable[k])
+			end
+		end
+	end
+	walk(instance.chart, instance.schema)
 end
 
 return validator
