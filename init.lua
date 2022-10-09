e2e68896bf6316788502867cf0d74b547b5a4560
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-10 01:20:36 +0200

    Init all classes

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 local CHIMES = {}
 
 CHIMES.Biomes = require("tew.CHIMES.components.Biomes")
+CHIMES.Explore = require("tew.CHIMES.components.Explore")
+CHIMES.Exteriors = require("tew.CHIMES.components.Exteriors")
+CHIMES.Interiors = require("tew.CHIMES.components.Interiors")
+CHIMES.Taverns = require("tew.CHIMES.components.Taverns")
+CHIMES.Weathers = require("tew.CHIMES.components.Weathers")
 
 return CHIMES
