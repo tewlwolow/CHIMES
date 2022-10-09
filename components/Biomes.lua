0e966663e93669236178dcbb267af4705dc22157
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-09 19:11:43 +0200

    Better class flow

--- components/Biomes.lua
+++ components/Biomes.lua
@@ -3,8 +3,6 @@ local Chart = require("tew.CHIMES.components.Chart")
 
 ---@class CHIMESBiomesChart : CHIMESChart
 local Biomes = Class.create(Chart)
-Biomes:_init()
 Biomes.schema.data.item.strings = { type = "table", required = true }
-Biomes:validate()
 
 return Biomes
