794a4e51ad5637152ccf8a2ef5f69dfb83284524
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-09 18:30:04 +0200

    Subclass runner workflow

--- components/Biomes.lua
+++ components/Biomes.lua
@@ -1,8 +1,10 @@
-local Chart = require("tew.CHIMES.components.Chart")
 local Class = require("tew.CHIMES.util.Class")
+local Chart = require("tew.CHIMES.components.Chart")
 
 ---@class CHIMESBiomesChart : CHIMESChart
 local Biomes = Class.create(Chart)
+Biomes:_init()
 Biomes.schema.data.item.strings = { type = "table", required = true }
+Biomes:validate()
 
 return Biomes
