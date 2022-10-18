61993095bd0346a5f61572e5efc60e1bc11f93ec
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-10-18 03:04:11 +0200

    Error checking back in

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@
 
 local metadata = require("tew.CHIMES.metadata")
 local version = metadata.version
+local ErrorMessage = require("tew.CHIMES.ui.ErrorMessage")
+local schemaErrors = require("tew.CHIMES.util.schemaErrors")
 
 local function init()
     mwse.log("[CHIMES] Version "..version.." initialised.")
@@ -18,6 +20,14 @@ end
 event.register("modConfigReady", function()
     dofile("Data Files\\MWSE\\mods\\tew\\CHIMES\\ui\\mcm.lua")
 end)
+
+local function checkForErrors()
+    if not table.empty(schemaErrors) then
+        ErrorMessage.show(schemaErrors)
+    end
+end
+
+event.register (tes3.event.uiActivated, checkForErrors, { filter = "MenuOptions", doOnce = true, })
 
 
 event.register("initialized", init)
