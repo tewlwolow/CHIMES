d6c31e85ee680157708293416df616191aef5ff7
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-06-17 20:34:43 +0200

    EVENTS: Override musicSelectTrack event

--- Data Files/MWSE/mods/tew/CHIMES/controllers/central.lua
+++ Data Files/MWSE/mods/tew/CHIMES/controllers/central.lua
@@ -13,7 +13,8 @@ function central.check()
 	local controller = resolver.resolveController()
 	if controller then
 		currentItem = controller.item
-		if ((previousItem) and (previousItem.folder ~= currentItem.folder)) or not (previousItem) then
+		if not currentItem then return end
+		if (currentItem.disable) or ((previousItem) and (previousItem.folder ~= currentItem.folder)) or not (previousItem) then
 			debug.log(currentItem.folder)
 			if previousItem then
 				debug.log(previousItem.folder)
@@ -28,18 +29,21 @@ end
 
 function central.override(e)
 	if tes3.player.mobile.inCombat then return end
+	e.claim = true
 	local controller = resolver.resolveController()
 	if controller then
 		currentItem = controller.item
-		if ((previousItem) and (previousItem.folder ~= currentItem.folder)) or not (previousItem) then
+		if not currentItem then return end
+		if (currentItem.disable) or ((previousItem) and (previousItem.folder ~= currentItem.folder)) or not (previousItem) then
 			currentTrack = trackSelector.selectNew(currentItem, previousTrack)
-			music.play(currentTrack)
+			debug.log(currentTrack)
 			e.music = currentTrack
 			e.situation = tes3.musicSituation.explore
 			previousTrack = currentTrack
 			previousItem = currentItem
 		end
 	end
+	return false
 end
 
 function central.purge()
--- Data Files/MWSE/mods/tew/CHIMES/events.lua
+++ Data Files/MWSE/mods/tew/CHIMES/events.lua
@@ -4,7 +4,7 @@ local central = require("tew.CHIMES.controllers.central")
 
 event.register(tes3.event.cellChanged, central.check)
 event.register(tes3.event.weatherTransitionStarted, central.check)
--- event.register(tes3.event.musicSelectTrack, central.override)
+event.register(tes3.event.musicSelectTrack, central.override)
 event.register(tes3.event.combatStarted, central.purge)
 event.register(tes3.event.combatStop, central.check)
 event.register(tes3.event.load, central.purge)
