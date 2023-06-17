884184ba921af4a126dea7acaa4f5351d1ce26bf
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-06-17 20:35:38 +0200

    WEATHERS: Bugger off if not exterior

--- Data Files/MWSE/mods/tew/CHIMES/controllers/weathers.lua
+++ Data Files/MWSE/mods/tew/CHIMES/controllers/weathers.lua
@@ -5,17 +5,25 @@ local catalogue = require("tew.CHIMES.cache.catalogue")
 weathers.item = nil
 
 function weathers.isRelevant()
+	local cell = tes3.player.cell
+	if not cell then return end
+	if not cell.isOrBehavesAsExterior then return end
+
 	local currentWeather = tes3.getCurrentWeather().name:lower()
 	local wc = tes3.worldController.weatherController
 	if not wc then return end
 	local nextWeather = wc.nextWeather and wc.nextWeather.name:lower()
 	local relevantWeather = nextWeather or currentWeather
 
+	debug.log(currentWeather)
+	debug.log(nextWeather)
+	debug.log(relevantWeather)
+
 	local charts = catalogue.CHIMESWeathersChart
 	for _, chart in ipairs(charts) do
 		for _, item in ipairs(chart.data) do
 			if item.id:lower() == relevantWeather then
-				weathers.item = item.item
+				weathers.item = item
 				return true
 			end
 		end
