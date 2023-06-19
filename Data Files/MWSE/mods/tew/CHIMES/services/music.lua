a6aaca6a511f3e1ed696b3f99462d8ed2691f6d4
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-06-20 00:50:04 +0200

    MUSIC: A bit crossfade for testing

--- Data Files/MWSE/mods/tew/CHIMES/services/music.lua
+++ Data Files/MWSE/mods/tew/CHIMES/services/music.lua
@@ -3,7 +3,8 @@ local music = {}
 function music.play(track)
 	tes3.streamMusic{
 		path = track,
-		situation = tes3.musicSituation.explore
+		situation = tes3.musicSituation.explore,
+		crossfade = 5
 	}
 end
 
